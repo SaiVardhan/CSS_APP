beb0a3e6af116b3f00bb5378787e5135b1d6d6ce
SaiVardhan <saimca03@gmail.com>
2016-04-19 14:41:27 +0530

    fixed indentation issue

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,35 +1,31 @@
 class PagesController < ApplicationController
 
-	def index
-		if current_user
-			@pages =  current_user.pages
-		end
-		@page = Page.new
-		respond_to do |format|
-			format.html
-		end
-	end
+  def index
+    @page = Page.new
+    respond_to do |format|
+      format.html
+    end
+  end
 
-	def create
-		@page = current_user.pages.build page_params
-		if @page.save
-			respond_to do |format|
-				format.js
-			end
-		else
-			render :action => 'new'
-		end
-	end
+  def create
+    @page = current_user.pages.build page_params
+    if @page.save
+    	respond_to do |format|
+        format.js
+    	end
+    else
+      render :action => 'new'
+    end
+  end
 
-	def new
-		@page = Page.new
-	end
+  def new
+    @page = Page.new
+  end
 
 
-	private
+  private
 
-	def page_params
-		params.require(:page).permit(:bg_color, :font)
-	end
-
+  def page_params
+    params.require(:page).permit(:bg_color, :font)
+  end
 end
